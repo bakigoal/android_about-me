eacc735fd584a1dd488ed871b4e5326bf855f472
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-08 11:21:56 +0500

    add button onclick listener

--- app/src/main/java/com/bakigoal/aboutme/MainActivity.kt
+++ app/src/main/java/com/bakigoal/aboutme/MainActivity.kt
@@ -2,10 +2,36 @@ package com.bakigoal.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var doneButton: Button
+    private lateinit var nicknameText: TextView
+    private lateinit var nicknameEdit: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doneButton = findViewById(R.id.done_button)
+        nicknameText = findViewById(R.id.nickname_text)
+        nicknameEdit = findViewById(R.id.nickname_edit)
+
+        doneButton.setOnClickListener { updateNickname() }
+    }
+
+    private fun updateNickname() {
+        val nickname = nicknameEdit.text
+        if (nickname.isNotEmpty()) {
+            nicknameText.text = nickname
+            nicknameText.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+        }
     }
 }
