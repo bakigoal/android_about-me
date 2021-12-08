a91cd25260cad40974bc0584038e45cebad48c46
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-08 11:28:49 +0500

    hide keyboard after setting nickname

--- app/src/main/java/com/bakigoal/aboutme/MainActivity.kt
+++ app/src/main/java/com/bakigoal/aboutme/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.bakigoal.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,16 +23,22 @@ class MainActivity : AppCompatActivity() {
         nicknameText = findViewById(R.id.nickname_text)
         nicknameEdit = findViewById(R.id.nickname_edit)
 
-        doneButton.setOnClickListener { updateNickname() }
+        doneButton.setOnClickListener { setNickname() }
     }
 
-    private fun updateNickname() {
+    private fun setNickname() {
         val nickname = nicknameEdit.text
         if (nickname.isNotEmpty()) {
             nicknameText.text = nickname
             nicknameText.visibility = View.VISIBLE
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
+            hideKeyboard(doneButton)
         }
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
